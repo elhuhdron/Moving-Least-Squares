65f7842b02d4bf082a79adb9c7fdf4d065cf2ed4
Paul Watkins <pwatkins@gmail.com>
2021-06-09 17:33:22 +0200

    try separating transform from warping

--- img_utils.py
+++ img_utils.py
@@ -8,18 +8,18 @@ Image deformation using moving least squares.
     * Similarity deformation
     * Rigid deformation
 
-For more details please reference the documentation: 
-    
+For more details please reference the documentation:
+
     Moving-Least-Squares/doc/Image Deformation.pdf
 
-or the original paper: 
-    
+or the original paper:
+
     Image deformation using moving least squares
-    Schaefer, Mcphail, Warren. 
+    Schaefer, Mcphail, Warren.
 
 Note:
     In the original paper, the author missed the weight w_j in formular (5).
-    In addition, all the formulars in section 2.1 miss the w_j. 
+    In addition, all the formulars in section 2.1 miss the w_j.
     And I have corrected this point in my documentation.
 
 @author: Jarvis ZHANG
@@ -28,12 +28,15 @@ Note:
 """
 
 import numpy as np
-from skimage.transform import rescale
+#from skimage.transform import rescale
+
+import scipy.ndimage as nd
+import scipy.interpolate as interp
 
 np.seterr(divide='ignore', invalid='ignore')
 
 def mls_affine_deformation_1pt(p, q, v, alpha=1):
-    ''' Calculate the affine deformation of one point.   
+    ''' Calculate the affine deformation of one point.
     This function is used to test the algorithm.
     '''
     ctrls = p.shape[0]
@@ -101,13 +104,13 @@ def mls_affine_deformation(image, p, q, alpha=1.0, density=1.0):
     reshaped_phat2 = phat.reshape(ctrls, 1, 2, grow, gcol)                              # [ctrls, 1, 2, grow, gcol]
     reshaped_w = w.reshape(ctrls, 1, 1, grow, gcol)                                     # [ctrls, 1, 1, grow, gcol]
     pTwp = np.sum(reshaped_phat1 * reshaped_w * reshaped_phat2, axis=0)                 # [2, 2, grow, gcol]
-    try:                
+    try:
         inv_pTwp = np.linalg.inv(pTwp.transpose(2, 3, 0, 1))                            # [grow, gcol, 2, 2]
-        flag = False                
-    except np.linalg.linalg.LinAlgError:                
-        flag = True             
+        flag = False
+    except np.linalg.linalg.LinAlgError:
+        flag = True
         det = np.linalg.det(pTwp.transpose(2, 3, 0, 1))                                 # [grow, gcol]
-        det[det < 1e-8] = np.inf                
+        det[det < 1e-8] = np.inf
         reshaped_det = det.reshape(1, 1, grow, gcol)                                    # [1, 1, grow, gcol]
         adjoint = pTwp[[[1, 0], [1, 0]], [[1, 1], [0, 0]], :, :]                        # [2, 2, grow, gcol]
         adjoint[[0, 1], [1, 0], :, :] = -adjoint[[0, 1], [1, 0], :, :]                  # [2, 2, grow, gcol]
@@ -140,7 +143,7 @@ def mls_affine_deformation(image, p, q, alpha=1.0, density=1.0):
 
     # Mapping original image
     transformed_image = np.ones_like(image) * 255
-    new_gridY, new_gridX = np.meshgrid((np.arange(gcol) / density).astype(np.int16), 
+    new_gridY, new_gridX = np.meshgrid((np.arange(gcol) / density).astype(np.int16),
                                         (np.arange(grow) / density).astype(np.int16))
     # transformed_image[tuple(transformers.astype(np.int16))] = image[new_gridX, new_gridY]    # [grow, gcol]
     transformed_image[new_gridX, new_gridY] = image[tuple(transformers.astype(np.int16))]
@@ -179,7 +182,7 @@ def mls_similarity_deformation(image, p, q, alpha=1.0, density=1.0):
     # Compute
     reshaped_p = p.reshape(ctrls, 2, 1, 1)                                              # [ctrls, 2, 1, 1]
     reshaped_v = np.vstack((vx.reshape(1, grow, gcol), vy.reshape(1, grow, gcol)))      # [2, grow, gcol]
-    
+
     w = 1.0 / np.sum((reshaped_p - reshaped_v) ** 2, axis=1)**alpha                     # [ctrls, grow, gcol]
     sum_w = np.sum(w, axis=0)                                                           # [grow, gcol]
     pstar = np.sum(w * reshaped_p.transpose(1, 0, 2, 3), axis=1) / sum_w                # [2, grow, gcol]
@@ -187,22 +190,22 @@ def mls_similarity_deformation(image, p, q, alpha=1.0, density=1.0):
     reshaped_phat1 = phat.reshape(ctrls, 1, 2, grow, gcol)                              # [ctrls, 1, 2, grow, gcol]
     reshaped_phat2 = phat.reshape(ctrls, 2, 1, grow, gcol)                              # [ctrls, 2, 1, grow, gcol]
     reshaped_w = w.reshape(ctrls, 1, 1, grow, gcol)                                     # [ctrls, 1, 1, grow, gcol]
-    mu = np.sum(np.matmul(reshaped_w.transpose(0, 3, 4, 1, 2) * 
-                          reshaped_phat1.transpose(0, 3, 4, 1, 2), 
+    mu = np.sum(np.matmul(reshaped_w.transpose(0, 3, 4, 1, 2) *
+                          reshaped_phat1.transpose(0, 3, 4, 1, 2),
                           reshaped_phat2.transpose(0, 3, 4, 1, 2)), axis=0)             # [grow, gcol, 1, 1]
     reshaped_mu = mu.reshape(1, grow, gcol)                                             # [1, grow, gcol]
     neg_phat_verti = phat[:, [1, 0],...]                                                # [ctrls, 2, grow, gcol]
-    neg_phat_verti[:, 1,...] = -neg_phat_verti[:, 1,...]                                
+    neg_phat_verti[:, 1,...] = -neg_phat_verti[:, 1,...]
     reshaped_neg_phat_verti = neg_phat_verti.reshape(ctrls, 1, 2, grow, gcol)           # [ctrls, 1, 2, grow, gcol]
     mul_left = np.concatenate((reshaped_phat1, reshaped_neg_phat_verti), axis=1)        # [ctrls, 2, 2, grow, gcol]
     vpstar = reshaped_v - pstar                                                         # [2, grow, gcol]
     reshaped_vpstar = vpstar.reshape(2, 1, grow, gcol)                                  # [2, 1, grow, gcol]
     neg_vpstar_verti = vpstar[[1, 0],...]                                               # [2, grow, gcol]
-    neg_vpstar_verti[1,...] = -neg_vpstar_verti[1,...]                                  
+    neg_vpstar_verti[1,...] = -neg_vpstar_verti[1,...]
     reshaped_neg_vpstar_verti = neg_vpstar_verti.reshape(2, 1, grow, gcol)              # [2, 1, grow, gcol]
     mul_right = np.concatenate((reshaped_vpstar, reshaped_neg_vpstar_verti), axis=1)    # [2, 2, grow, gcol]
     reshaped_mul_right = mul_right.reshape(1, 2, 2, grow, gcol)                         # [1, 2, 2, grow, gcol]
-    A = np.matmul((reshaped_w * mul_left).transpose(0, 3, 4, 1, 2), 
+    A = np.matmul((reshaped_w * mul_left).transpose(0, 3, 4, 1, 2),
                        reshaped_mul_right.transpose(0, 3, 4, 1, 2))                     # [ctrls, grow, gcol, 2, 2]
 
      # Calculate q
@@ -223,7 +226,7 @@ def mls_similarity_deformation(image, p, q, alpha=1.0, density=1.0):
 
     # Mapping original image
     transformed_image = np.ones_like(image) * 255
-    new_gridY, new_gridX = np.meshgrid((np.arange(gcol) / density).astype(np.int16), 
+    new_gridY, new_gridX = np.meshgrid((np.arange(gcol) / density).astype(np.int16),
                                         (np.arange(grow) / density).astype(np.int16))
     # transformed_image[tuple(transformers.astype(np.int16))] = image[new_gridX, new_gridY]    # [grow, gcol]
     transformed_image[new_gridX, new_gridY] = image[tuple(transformers.astype(np.int16))]
@@ -262,7 +265,7 @@ def mls_rigid_deformation(image, p, q, alpha=1.0, density=1.0):
     # Compute
     reshaped_p = p.reshape(ctrls, 2, 1, 1)                                              # [ctrls, 2, 1, 1]
     reshaped_v = np.vstack((vx.reshape(1, grow, gcol), vy.reshape(1, grow, gcol)))      # [2, grow, gcol]
-    
+
     w = 1.0 / np.sum((reshaped_p - reshaped_v) ** 2, axis=1)**alpha                     # [ctrls, grow, gcol]
     sum_w = np.sum(w, axis=0)                                                           # [grow, gcol]
     pstar = np.sum(w * reshaped_p.transpose(1, 0, 2, 3), axis=1) / sum_w                # [2, grow, gcol]
@@ -270,17 +273,17 @@ def mls_rigid_deformation(image, p, q, alpha=1.0, density=1.0):
     reshaped_phat = phat.reshape(ctrls, 1, 2, grow, gcol)                               # [ctrls, 1, 2, grow, gcol]
     reshaped_w = w.reshape(ctrls, 1, 1, grow, gcol)                                     # [ctrls, 1, 1, grow, gcol]
     neg_phat_verti = phat[:, [1, 0],...]                                                # [ctrls, 2, grow, gcol]
-    neg_phat_verti[:, 1,...] = -neg_phat_verti[:, 1,...]                                
+    neg_phat_verti[:, 1,...] = -neg_phat_verti[:, 1,...]
     reshaped_neg_phat_verti = neg_phat_verti.reshape(ctrls, 1, 2, grow, gcol)           # [ctrls, 1, 2, grow, gcol]
     mul_left = np.concatenate((reshaped_phat, reshaped_neg_phat_verti), axis=1)         # [ctrls, 2, 2, grow, gcol]
     vpstar = reshaped_v - pstar                                                         # [2, grow, gcol]
     reshaped_vpstar = vpstar.reshape(2, 1, grow, gcol)                                  # [2, 1, grow, gcol]
     neg_vpstar_verti = vpstar[[1, 0],...]                                               # [2, grow, gcol]
-    neg_vpstar_verti[1,...] = -neg_vpstar_verti[1,...]                                  
+    neg_vpstar_verti[1,...] = -neg_vpstar_verti[1,...]
     reshaped_neg_vpstar_verti = neg_vpstar_verti.reshape(2, 1, grow, gcol)              # [2, 1, grow, gcol]
     mul_right = np.concatenate((reshaped_vpstar, reshaped_neg_vpstar_verti), axis=1)    # [2, 2, grow, gcol]
     reshaped_mul_right = mul_right.reshape(1, 2, 2, grow, gcol)                         # [1, 2, 2, grow, gcol]
-    A = np.matmul((reshaped_w * mul_left).transpose(0, 3, 4, 1, 2), 
+    A = np.matmul((reshaped_w * mul_left).transpose(0, 3, 4, 1, 2),
                        reshaped_mul_right.transpose(0, 3, 4, 1, 2))                     # [ctrls, grow, gcol, 2, 2]
 
     # Calculate q
@@ -300,12 +303,103 @@ def mls_rigid_deformation(image, p, q, alpha=1.0, density=1.0):
     transformers[transformers < 0] = 0
     transformers[0][transformers[0] > height - 1] = 0
     transformers[1][transformers[1] > width - 1] = 0
-    
+
     # Mapping original image
     transformed_image = np.ones_like(image) * 255
-    new_gridY, new_gridX = np.meshgrid((np.arange(gcol) / density).astype(np.int16), 
+    new_gridY, new_gridX = np.meshgrid((np.arange(gcol) / density).astype(np.int16),
                                         (np.arange(grow) / density).astype(np.int16))
     # transformed_image[tuple(transformers.astype(np.int16))] = image[new_gridX, new_gridY]    # [grow, gcol]
     transformed_image[new_gridX, new_gridY] = image[tuple(transformers.astype(np.int16))]
-    
+
     return transformed_image
+
+def mls_rigid_transform(v, p, q, alpha=1.0):
+    ''' Rigid transform
+    ### Params:
+        * v - ndarray: an array with size [m, 2], points to transform
+        * p - ndarray: an array with size [n, 2], control points
+        * q - ndarray: an array with size [n, 2], deformed control points
+        * alpha - float: parameter used by weights
+    ### Return:
+        f_r - ndarray: an array with size [m, 2], deformed points from v
+    '''
+
+    m, n = v.shape[0], p.shape[0]
+    assert( q.shape[0] == n )
+
+    # shape (n,m,2)
+    p_ij = p[:,None,:]
+    q_ij = q[:,None,:]
+    v_ij = v[None,:,:]
+
+    # weights, shape (n,m)
+    w = 1.0 / np.sum((p_ij - v_ij)**2, axis=2)**alpha
+    w_ij = w[:,:,None]
+    sum_w = np.sum(w, axis=0)
+    #sum_w_ij = sum_w[None,:,None]
+
+    # centroids
+    p_star = (w_ij * p_ij).sum(0) / sum_w[:,None] # shape (m,2)
+    q_star = (w_ij * q_ij).sum(0) / sum_w[:,None] # shape (m,2)
+    p_star_ij = p_star[None,:,:]
+    q_star_ij = q_star[None,:,:]
+    p_hat_ij = p_ij - p_star_ij
+    q_hat_ij = q_ij - q_star_ij
+
+    # similarity transform solved matrix
+    p_hat_ij_x = np.zeros_like(p_hat_ij)
+    # from paper: (_x) is an operator on 2D vectors such that (x, y) = (−y, x).
+    p_hat_ij_x[:,:,0] = -p_hat_ij[:,:,1]
+    p_hat_ij_x[:,:,1] = p_hat_ij[:,:,0]
+    v_minus_p_star = v_ij - p_star_ij
+    v_minus_p_star_x = np.zeros_like(v_minus_p_star)
+    v_minus_p_star_x[:,:,0] = -v_minus_p_star[:,:,1]
+    v_minus_p_star_x[:,:,1] = v_minus_p_star[:,:,0]
+    A_ij = w_ij[:,:,:,None] * np.matmul(\
+            np.concatenate((p_hat_ij[:,:,:,None], -p_hat_ij_x[:,:,:,None]), axis=3),
+            np.concatenate((v_minus_p_star[:,:,:,None], -v_minus_p_star_x[:,:,:,None]), axis=3).transpose(0,1,3,2),
+            )
+
+    # f_r, shape (m,2)
+    f_bar_r = np.matmul(q_hat_ij[:,:,None,:], A_ij).sum(0).reshape(m,2)
+    v_minus_p_star = v_minus_p_star.reshape(m,2)
+    f_r = np.sqrt((v_minus_p_star**2).sum(1))[:,None] * f_bar_r / np.sqrt((f_bar_r**2).sum(0))[:,None] + q_star
+
+    return f_r
+
+def create_swirl_deltas(img_shape, warp_rs):
+    spacing = np.pi/(min(img_shape)/2.5)
+    nimages = len(warp_rs)
+
+    #x,y = np.mgrid[:img_shape[0]:50,:img_shape[1]:50]
+    x,y = np.meshgrid(np.linspace(0,img_shape[0]-1,img_shape[0]//20),
+                      np.linspace(0,img_shape[1]-1,img_shape[1]//20))
+    x = x.flat[:]; y = y.flat[:]
+    xc = x - x.mean(0); yc = y - y.mean(0)
+    pts = np.concatenate((x[:,None], y[:,None]), axis=1)
+
+    deltas = [None]*nimages
+    for i in range(nimages):
+        deltas[i] = warp_rs[i]*np.concatenate((np.sin(yc*spacing)[:,None], -np.sin(xc*spacing)[:,None]), axis=1)
+
+    return pts, deltas
+
+def warp_image_remap_interp(img, pts, deltas):
+    img_shape = img.shape
+    # img_dtype = img.dtype
+
+    # create a series of warped images
+    grid_x, grid_y = np.indices((img_shape[0], img_shape[1]), dtype=np.double)
+
+    vx = interp.griddata(pts, deltas[:,0], (grid_x, grid_y), fill_value=0., method='cubic')
+    vy = interp.griddata(pts, deltas[:,1], (grid_x, grid_y), fill_value=0., method='cubic')
+    coords = [vx+grid_x, vy+grid_y]
+    image = nd.map_coordinates(img, coords, order=1, mode='constant', cval=0.0, prefilter=False)
+
+    rdeltas = -deltas
+    vx = interp.griddata(pts, rdeltas[:,0], (grid_x, grid_y), fill_value=0., method='cubic')
+    vy = interp.griddata(pts, rdeltas[:,1], (grid_x, grid_y), fill_value=0., method='cubic')
+    coords = [vx+grid_x, vy+grid_y]
+    rimage = nd.map_coordinates(image, coords, order=1, mode='constant', cval=0.0, prefilter=False)
+
+    return image, rimage
--- img_utils_demo.py
+++ img_utils_demo.py
@@ -14,6 +14,18 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from img_utils import mls_affine_deformation, mls_similarity_deformation, mls_rigid_deformation
+from img_utils import mls_rigid_transform, create_swirl_deltas, warp_image_remap_interp
+
+from PIL import Image
+
+p_mr_big = np.array([
+    [30, 155], [125, 155], [225, 155],
+    [100, 235], [160, 235], [85, 295], [180, 293]
+])
+q_mr_big = np.array([
+    [42, 211], [125, 155], [235, 100],
+    [80, 235], [140, 235], [85, 295], [180, 295]
+])
 
 
 def show_example():
@@ -22,22 +34,16 @@ def show_example():
     plt.show()
 
 def demo(fun, name):
-    p = np.array([
-        [30, 155], [125, 155], [225, 155],
-        [100, 235], [160, 235], [85, 295], [180, 293]
-    ])
-    q = np.array([
-        [42, 211], [125, 155], [235, 100],
-        [80, 235], [140, 235], [85, 295], [180, 295]
-    ])
     image = plt.imread(os.path.join(sys.path[0], "mr_big_ori.jpg"))
+    p = p_mr_big
+    q = q_mr_big
 
     plt.figure(figsize=(8, 3))
     plt.subplot(131)
     plt.axis('off')
     plt.imshow(image)
     plt.title("Original Image")
-    
+
     transformed_image = fun(image, p, q, alpha=1, density=1)
     plt.subplot(132)
     plt.axis('off')
@@ -50,19 +56,19 @@ def demo(fun, name):
     plt.title("%s Deformation \n Sampling density 0.7"%name)
 
     plt.tight_layout(w_pad=0.1)
-    plt.show()
+    #plt.show()
 
 def demo2(fun, name):
-    ''' 
-        Smiled Monalisa  
     '''
-    
+        Smiled Monalisa
+    '''
+
     p = np.array([
-        [186, 140], [295, 135], [208, 181], [261, 181], [184, 203], [304, 202], [213, 225], 
+        [186, 140], [295, 135], [208, 181], [261, 181], [184, 203], [304, 202], [213, 225],
         [243, 225], [211, 244], [253, 244], [195, 254], [232, 281], [285, 252]
     ])
     q = np.array([
-        [186, 140], [295, 135], [208, 181], [261, 181], [184, 203], [304, 202], [213, 225], 
+        [186, 140], [295, 135], [208, 181], [261, 181], [184, 203], [304, 202], [213, 225],
         [243, 225], [207, 238], [261, 237], [199, 253], [232, 281], [279, 249]
     ])
     image = plt.imread(os.path.join(sys.path[0], "monalisa.jpg"))
@@ -79,12 +85,51 @@ def demo2(fun, name):
     plt.tight_layout(w_pad=1.0, h_pad=1.0)
     plt.show()
 
+def demo3(fn, name, p=None, q=None):
+    image = plt.imread(fn)
+    print(image.shape)
+
+    if p is None:
+        #warp_rs = [0, 5, 10, 15, 20, 15, 10, 5, 0]
+        warp_rs = [20]
+        pts, deltas = create_swirl_deltas(image.shape, warp_rs)
+        deltas = deltas[0]
+        p = pts; q = pts + deltas
+    else:
+        pts = p; deltas = q - p
+
+    plt.figure(figsize=(13, 4))
+    plt.subplot(131)
+    plt.axis('off')
+    plt.imshow(image, cmap='gray')
+    plt.title("Original Image")
+
+    transformed_image1 = warp_image_remap_interp(image, pts, deltas)
+    plt.subplot(132)
+    plt.axis('off')
+    plt.imshow(transformed_image1, cmap='gray')
+    plt.title("Swirl Deformation \n")
+
+    # transformed_image = fun(image, p, q, alpha=1, density=0.7)
+    # plt.subplot(133)
+    # plt.axis('off')
+    # plt.imshow(transformed_image)
+    # plt.title("%s Deformation \n Sampling density 0.7"%name)
+
+    plt.tight_layout(w_pad=0.1)
+    #plt.show()
+
 
 if __name__ == "__main__":
-    demo(mls_affine_deformation, "Affine")
-    demo(mls_similarity_deformation, "Similarity")
-    demo(mls_rigid_deformation, "Rigid")
+    # demo(mls_affine_deformation, "Affine")
+    # demo(mls_similarity_deformation, "Similarity")
+    #demo(mls_rigid_deformation, "Rigid")
 
-    demo2(mls_affine_deformation, "Affine")
-    demo2(mls_similarity_deformation, "Similarity")
-    demo2(mls_rigid_deformation, "Rigid")
+    fn = '50426488-complex-maze-puzzle-game-for-leisure-time-with-very-high-level-of-difficulty.jpeg'
+    demo3(fn, "New Rigid")
+
+    plt.show()
+
+    # demo2(mls_affine_deformation, "Affine")
+    # demo2(mls_similarity_deformation, "Similarity")
+    # demo2(mls_rigid_deformation, "Rigid")
